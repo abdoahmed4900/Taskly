9d28332e3c1df4d05135edce698d3eaf56e61f2d
abdoahmed4900 <abdo.ahmed4900@gmail.com>
2026-06-29 05:45:32 +0300

    fix:change environment file structure to object

--- src/app/core/interceptor/url.interceptor.ts
+++ src/app/core/interceptor/url.interceptor.ts
@@ -1,10 +1,10 @@
-import { apiKey, baseUrl } from './../../env/environment';
+import { environment } from './../../environments/environment';
 import { HttpHandlerFn, HttpRequest } from '@angular/common/http';
 
 export function urlInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn) {
   const newReq = req.clone({
-    url: `${baseUrl}/${req.url}`,
-    headers: req.headers.append('apikey', apiKey),
+    url: `${environment.baseUrl}/${req.url}`,
+    headers: req.headers.append('apikey', environment.apiKey),
   });
   return next(newReq);
 }
