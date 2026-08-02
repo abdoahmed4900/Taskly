6e3e6e6e2d8a13a0c08d5b590d49d18003415346
abdoahmed4900 <abdo.ahmed4900@gmail.com>
2026-08-02 17:16:07 +0300

    fix : remove extra slash in application url in project invitation api func

--- src/app/features/members/service/members.api.service.ts
+++ src/app/features/members/service/members.api.service.ts
@@ -21,7 +21,7 @@ export class MembersApiService {
     return this.httpClient.post(`rest/v1/rpc/invite_member`, {
       p_email: email,
       p_project_id: projectId,
-      p_app_url: 'https://taskly-lime-nine.vercel.app/',
+      p_app_url: 'https://taskly-lime-nine.vercel.app',
       p_base_url: environment.baseUrl,
     });
   }
